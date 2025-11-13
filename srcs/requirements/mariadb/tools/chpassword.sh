3b7c5a29401d23e7b4530c49d3baae33284a519e
Kristaps Zarins <kris@Kristapss-MacBook-Air.local>
2025-11-13 10:57:33 +0100

    Add improvements to the mariadb init script

--- srcs/requirements/mariadb/tools/chpassword.sh
+++ srcs/requirements/mariadb/tools/chpassword.sh
@@ -9,17 +9,21 @@
 #Should be changed for container dir ->
 #db_root_password=$(cat $HOME/data/secrets/db_root_password.txt)
 #db_root_password=$(cat /secrets/db_root_password.txt)
-db_root_password='bob'
 
+#TODO:Remove the hard coded test password
+db_root_password='bob'
 db_password='cat'
+
 #db_password=$(cat /secrets/db_password.txt)
+#TODO: change the hostname to more precise definition
 wp_hostname='%'
 
+#TODO: remove when used for prod
 echo $db_root_password
 echo $db_password
 
 #The script to run should be here
-echo mysql -h localhost -uroot <<"EOF"
+mysql -h localhost -uroot <<"EOF"
 CREATE USER '${MYSQL_USER}'@'${wp_hostname}' IDENTIFIED BY ${db_password};
 ALTER USER 'root'@'localhost' IDENTIFIED BY ${db_root_password};
 CREATE DATABASE wordpress;
@@ -30,5 +34,19 @@ EOF
 #Test to see the input of the command
 #echo "CREATE USER '${MYSQL_USER}'@'${wp_hostname}' IDENTIFIED BY ${db_password}; ALTER USER 'root'@'localhost' IDENTIFIED BY ${db_root_password}; CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO '${MYSQL_USER}'@'${wp_hostname}';FLUSH PERMISIONS;"
 
-#exec 
-#exit
+chpasswd << "EOF"
+sudo:${db_root_password}
+mysql:${db_password}
+EOF
+
+/etc/init.d/mariadb stop
+su - mysql
+
+#TODO: check if HOME is not set
+export MARIADB_HOME=${HOME}
+#TODO: change the bind-address to the hostname of the wp_hostname
+echo $'[mariadbd]\n\
+bind-address = 0.0.0.0'\
+> ${MARIADB_HOME}/my.cnf
+
+exec mariadbd
